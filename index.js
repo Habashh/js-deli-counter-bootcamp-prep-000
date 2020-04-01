583eb54563177cb65649e41cf493b01f884e0f7b
zaid85j <zaid85j@gmail.com>
2020-04-01 19:51:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ var katzDeliLine = ["Bill", "Jane", "Ann"];
 
 var katzDeliLine = ["Steven", "Blake", "Avi"];
 
-function currentLine(katzDeliLine){
-if(katzDeliLine.length>0){
-for(var i=0;i<katzDeliLine.length;i++){
-var currentLinePerson= katzDeliLine.shift()
- console.log(`The line is currently: ${i}. ${katzDeliLine[i]} , ${i+1}.${katzDeliLine[i+1]}`)
-return katzDeliLine}
-}else{return "The line is currently empty."}
+var katzDeliLine = [];
+function currentLine(katzDeliLine) {
+    var output = [];
+    if (katzDeliLine.length > 0) {
+      for(var i = 0; i < katzDeliLine.length; i++) {
+        output += (i + 1) + ". " + katzDeliLine[i] + ", ";
+      }
+      output = output.slice(0, output.length - 2);
+      return "The line is currently: " + output;
+    } else {
+        return "The line is currently empty.";
+    }
 }
